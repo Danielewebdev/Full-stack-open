0fa50c75390ff97aa6c53f54fc73fa35c6148d87
cirnobestgirl <danielebraccini98@gmail.com>
2024-09-20 11:50:54 +0200

    Exercise 3.3, 3.4, 3.5, 3.6

--- part3/index.js
+++ part3/index.js
@@ -26,8 +26,66 @@ let persons = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  response.send({ persons });
+// Get single phonebook entry
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// Delete a single phonebook entry
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+// Generating new ID for post requests
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+// Adding new phonebook entries with HTTP POST requests
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  // Check if name already exists before adding to phonebook
+  const existingPerson = persons.find((person) => person.name === body.name);
+  if (existingPerson) {
+    return response.status(400).json({
+      error: "name already exists",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 // Route to get phonebook info and request info
